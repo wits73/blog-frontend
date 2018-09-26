2f657b0efae1068271103c48829769226f76a2be
Tae Kim <tkim25@kennesaw.edu>
2018-09-26 15:54:03 -0400

    added generate-react-component

---
+++ src/components/common/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import styles from './Footer.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        Footer
+      </div>
+    );
+  }
+}
+
+
+export default Footer;
---
+++ src/components/common/Header/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import styles from './Header.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        Header
+      </div>
+    );
+  }
+}
+
+
+export default Header;
