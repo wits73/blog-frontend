3d8225d7878d028be99ab4e5232ce5d6f3d4ad33
Tae Kim <tkim25@kennesaw.edu>
2018-09-28 22:47:14 -0400

    added header Footer

--- src/components/common/Footer/Footer.js
+++ src/components/common/Footer/Footer.js
@@ -2,19 +2,16 @@ import React, { Component } from 'react';
 
 import styles from './Footer.scss';
 import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
-
-class Footer extends Component {
-  render() {
-    return (
-      <div>
-        Footer
-      </div>
-    );
-  }
-}
+const Footer = () => (
+  <footer className={cx('footer')}>
+    <Link to="/" className={cx('brand')}>reactblog</Link>
+    <div className={cx('admin-login')}>Admin login</div>
+  </footer>
+)
 
 
 export default Footer;
--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 import styles from './Header.scss';
 import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
-
-class Header extends Component {
-  render() {
-    return (
-      <div>
-        Header
+const Header = () => (
+  <header className={cx('header')}>
+    <div className={cx('header-content')}>
+      <div className={cx('brand')}>
+        <Link to="/">reactblog</Link>
       </div>
-    );
-  }
-}
+      <div className={cx('right')}>
+        RIGHT
+      </div>
+    </div>
+  </header>
+);
+
 
 
 export default Header;
